bd8fe18c16c918b0fa89d3d4125bfcbe4aadf086
Maxah <maxime.joly96@gmail.com>
2024-04-16 22:06:41 +0200

    Title Screen

---
+++ maze_project/Assets/Scripts/TitleScreen/MenuOption.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace maze_game.TitleScreen
+{
+    public class MenuOption : MonoBehaviour
+    {
+        public enum AvailableOption { NewGame, LoadGame }
+
+        [SerializeField]
+        private Transform _cursor;
+        [SerializeField]
+        private AvailableOption _option;
+
+        public AvailableOption Option { get { return _option; } }
+
+        #region Unity Methods
+        private void Awake()
+        {
+            if(!_cursor)
+            {
+                _cursor = transform.Find("Cursor");
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void Select()
+        {
+            _cursor.gameObject.SetActive(true);
+        }
+
+        public void Unselect()
+        {
+            _cursor.gameObject.SetActive(false);
+        }
+        #endregion
+    }
+}
---
+++ maze_project/Assets/Scripts/TitleScreen/TitleScreenManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace maze_game.TitleScreen
+{
+    public class TitleScreenManager : MonoBehaviour
+    {
+        [SerializeField]
+        private MenuOption _newGameOption;
+
+        [SerializeField]
+        private MenuOption _loadGameOption;
+
+        public MenuOption.AvailableOption SelectedOption { get; private set; }
+
+        #region Unity Methods
+        private void Awake()
+        {
+            PutCursorOnNewGame();
+        }
+
+        private void Update()
+        {
+            if (Input.GetAxis("Vertical") > 0.0f)
+            {
+                PutCursorOnNewGame();
+            }
+
+            if (Input.GetAxis("Vertical") < 0.0f)
+            {
+                PutCursorOnLoadGame();
+            }
+
+            if(Input.GetButton("Submit"))
+            {
+                Debug.Log(SelectedOption);
+            }
+        }
+        #endregion
+
+        #region Methods
+        private void PutCursorOnNewGame()
+        {
+            _newGameOption.Select();
+            _loadGameOption.Unselect();
+
+            SelectedOption = _newGameOption.Option;
+        }
+
+        private void PutCursorOnLoadGame()
+        {
+            _newGameOption.Unselect();
+            _loadGameOption.Select();
+
+            SelectedOption = _loadGameOption.Option;
+        }
+        #endregion
+    }
+}
